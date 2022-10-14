6aa24d7962e628ced6072153387ffe7585b69b4b
Gizem Demirhan <gizemdemirhaan@goldbeck.de>
2022-10-14 18:02:33 +0200

    Table added, local IFC added

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,9 +1,19 @@
 import "./login.scss"
+import Sidebar from "../../components/sidebar/Sidebar"
+import Navbar from "../../components/navbar/Navbar"
+// import Container from "../../components/container/Container"
+import LoadLocalIFC from "../../components/loadLocalIFC/loadLocalIFC"
 
 const Login = () => {
     return (
         <div className="login"> 
-            login           
+            <Sidebar/>
+            <div className="homeContainer"> 
+                <Navbar/>
+                <div component>
+                    <LoadLocalIFC/>
+                </div>
+            </div>          
         </div>
     )
 }
