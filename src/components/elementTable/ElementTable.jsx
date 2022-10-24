6f3b08519cc4188a7ba630e6896a5efe97e45b5b
Gizem Demirhan <gizemdemirhaan@goldbeck.de>
2022-10-24 13:23:26 +0200

    statusPopover style

--- src/components/elementTable/ElementTable.jsx
+++ src/components/elementTable/ElementTable.jsx
@@ -9,28 +9,32 @@ import Paper from "@mui/material/Paper";
 
 const ElementTable = (props) => {
 
-    const data = props.data;
-    console.log(data);
+  const data = props.data;
+  
+  
+  console.log(data);
 
   return (
     <TableContainer component={Paper} className="table">
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell className="tableCell">ID</TableCell>
+            {/* <TableCell className="tableCell">ID</TableCell> */}
+            <TableCell className="tableCell">Type</TableCell>
             <TableCell className="tableCell">Global Id</TableCell>
-            <TableCell className="tableCell">Entity Type</TableCell>
             <TableCell className="tableCell">Name</TableCell>
+            <TableCell className="tableCell">Status</TableCell>
             <TableCell className="tableCell">Date</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {data.map((row) => (
             <TableRow key={row.id}>
-              <TableCell className="tableCell">{row['GlobalId']}</TableCell>
+              <TableCell className="tableCell">{row['PredefinedType']}</TableCell>
               <TableCell className="tableCell">{row['GlobalId']}</TableCell>
               <TableCell className="tableCell">{row['Name']}</TableCell>
-              <TableCell className="tableCell">{"12.03.2022"}</TableCell>
+              <TableCell className="tableCell">{"status"}</TableCell>
+              <TableCell className="tableCell">{"date"}</TableCell>
             </TableRow>
           ))}
         </TableBody>
--- src/components/statusPopover/StatusPopover.jsx
+++ src/components/statusPopover/StatusPopover.jsx
@@ -1,52 +1,68 @@
 import React from "react";
+import "./statusPopover.scss"
 import { Popover,Grid, Typography } from "@mui/material";
-
 import Button from '@mui/material/Button';
+//datepicker imports
 import TextField from '@mui/material/TextField';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 
 
 const StatusPopover = (props) => {
 
-    const id = props.id;
-    const open = props.open;
-    const anchorElem = props.anchorElem;
-    const handleClose = props.onClose;
-    const curIfcRecords = props.curIfcRecords;
-    const sendElementToTable = props.sendElementToTable;
+  const id = props.id;
+  const open = props.open;
+  const anchorElem = props.anchorElem;
+  const handleClose = props.onClose;
+  const curIfcRecords = props.curIfcRecords;
+  const sendElementToTable = props.sendElementToTable;
+  const [date, setDate] = React.useState(null);
 
-    return (
-        <Popover
-            id={id}
-            open={open}
-            anchorElem={anchorElem}
-            onClose={handleClose}
-            anchorOrigin={{
-              vertical: 'top',
-              horizontal: 'right',
-            }}
-          >
-            <Grid
-              container
-              component='dl'
-              spacing={2}
-              sx={{ p: 2 }}>
-              <Grid item>
-                {curIfcRecords && Object.keys(curIfcRecords).map((key) =>
-                  curIfcRecords[key] &&
-                  <>
-                    <React.Fragment key={key}>
-                      <Typography component='dt' variant='body2'>{key}</Typography>
-                      <Typography sx={{ pb: 1 }} component='dd'>{curIfcRecords[key]}</Typography>                    
-                    </React.Fragment>                        
-                  </>
-                )}
-              </Grid>
-              <TextField />
-              <Button onClick={sendElementToTable}>Ok</Button>
-            </Grid>
-          </Popover>
-    )
+  return (
+    <Popover className="statusPopover"
+      id={id}
+      open={open}
+      anchorElem={anchorElem}
+      onClose={handleClose}
+      anchorOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+    >
+      <Grid 
+        container
+        component='dl'
+        spacing={2}
+        sx={{ p: 2 }}
+        >
+          <Grid item>
+            {curIfcRecords && Object.keys(curIfcRecords).map((key) =>
+              curIfcRecords[key] &&
+              <>
+                <React.Fragment key={key}>
+                  <Typography component='dt' variant='body2'>{key}</Typography>
+                  <Typography sx={{ pb: 1 }} component='dd'>{curIfcRecords[key]}</Typography>                    
+                </React.Fragment>                        
+              </>
+            )}
+          </Grid>
+          <div className="bottomDiv">                 
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <DatePicker
+                label="Set Date"
+                value={date}
+                onChange={(newValue) => {
+                  setDate(newValue);
+                }}
+                renderInput={(params) => <TextField {...params} />}
+              />
+            </LocalizationProvider>
+            <Button  className="button" onClick={sendElementToTable}>Ok</Button>
+          </div> 
+      </Grid>
+    </Popover>
+  )
 }
 
 export {StatusPopover};
