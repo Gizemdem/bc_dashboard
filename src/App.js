35f7a34070ad80a1dcac2652f61d161cfdbe33ba
Gizemdem <69429638+Gizemdem@users.noreply.github.com>
2022-11-06 15:08:51 +0100

    in appjs pages route change to separeted

--- src/App.js
+++ src/App.js
@@ -5,26 +5,26 @@ import Login from "./pages/login/Login";
 import Welcome from "./pages/welcome/Welcome";
 import Account from "./pages/account/Account";
 import Projects from "./pages/projects/Projects";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 
 
 function App() {
     
   return (
     <div className="App">
-        <BrowserRouter>
+        <HashRouter>
           <Routes>
               <Route path="/" >
                   <Route index element = {<Home/>}/>
-                  <Route path="projects" element={<Projects/>}/>
-                  <Route path="dashboard" element = {<Dashboard/>}/>
-                  <Route path="welcome" element = {<Welcome/>}/>
-                  <Route path="register" element = {<Register/>}/>
-                  <Route path="login" element = {<Login/>}/>
-                  <Route path="account" element= {<Account/>}/>
               </Route>
+              <Route path="/projects" element={<Projects/>}/>
+              <Route path="/dashboard" element = {<Dashboard/>}/>
+              <Route path="/welcome" element = {<Welcome/>}/>
+              <Route path="/register" element = {<Register/>}/>
+              <Route path="/login" element = {<Login/>}/>
+              <Route path="/account" element= {<Account/>}/>
           </Routes>
-        </BrowserRouter>
+        </HashRouter>
     </div>
   );
 }
